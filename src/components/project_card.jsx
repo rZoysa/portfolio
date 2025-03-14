594022e605ac96dc74059c9cf110a3f2e8944f49
rZoysa <rithikzoysa@gmail.com>
2025-03-15 00:31:14 +0530

    add navigation functionality to ProjectCard component with custom cursor styles

--- src/components/project_card.jsx
+++ src/components/project_card.jsx
@@ -1,11 +1,25 @@
 import PropTypes from "prop-types";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
+import { useRef } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 export default function ProjectCard({ title, description, link, images }) {
+  const swiperRef = useRef(null);
+
+  // Function to navigate Swiper
+  const handleSlide = (direction) => {
+    if (swiperRef.current) {
+      if (direction === "left") {
+        swiperRef.current.slidePrev(); // Move to previous slide
+      } else {
+        swiperRef.current.slideNext(); // Move to next slide
+      }
+    }
+  };
+
   return (
     <div className="flex flex-col md:flex-row items-center bg-gray-800 rounded-lg shadow-lg">
       {/* Image Slider */}
@@ -16,15 +30,26 @@ export default function ProjectCard({ title, description, link, images }) {
           slidesPerView={1}
           autoplay={{ delay: 1000 }}
           pagination={{ clickable: false }}
+          onSwiper={(swiper) => (swiperRef.current = swiper)}
           className="rounded-bl-lg rounded-tl-lg"
         >
           {images.map((image, index) => (
-            <SwiperSlide key={index}>
+            <SwiperSlide key={index} className="relative">
               <img
                 src={image}
                 alt={`Slide ${index}`}
-                className="w-full h-64 object-cover cursor-grab active:cursor-grabbing"
+                className="w-full h-64 object-cover"
               />
+
+              <div
+                className="absolute top-0 left-0 h-full w-1/2 cursor-custom-left"
+                onClick={() => handleSlide("left")}
+              ></div>
+
+              <div
+                className="absolute top-0 right-0 h-full w-1/2 cursor-custom-right"
+                onClick={() => handleSlide("right")}
+              ></div>
             </SwiperSlide>
           ))}
         </Swiper>
